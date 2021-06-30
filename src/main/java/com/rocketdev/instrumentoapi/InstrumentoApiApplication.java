01ba3e9cc1bb7bd7ee3abd461e943f87d8d83ea9
Rafael <rafael.2018.ads@gmail.com>
2021-06-30 17:43:44 -0300

    Projeto iniciado

---
+++ src/main/java/com/rocketdev/instrumentoapi/InstrumentoApiApplication.java
@@ -0,0 +1,13 @@
+package com.rocketdev.instrumentoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstrumentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstrumentoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocketdev/instrumentoapi/InstrumentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocketdev.instrumentoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstrumentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
