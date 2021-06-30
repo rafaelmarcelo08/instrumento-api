2eaa386a9faa174fd85b786227fecfb8dd8648af
Rafael <rafael.2018.ads@gmail.com>
2021-06-30 19:49:01 -0300

    Criado o repository instrumento

--- src/main/java/com/rocketdev/instrumentoapi/repositories/InstrumentoRepository.java
+++ src/main/java/com/rocketdev/instrumentoapi/repositories/InstrumentoRepository.java
@@ -1,5 +1,13 @@
 package com.rocketdev.instrumentoapi.repositories;
 
-public class InstrumentoRepository {
+import org.springframework.data.repository.CrudRepository;
+import com.rocketdev.instrumentoapi.domain.Instrumento;
 
+/**
+ * Interface que define as operações da camada de persistencia de instrumento
+ * 
+ * @author Rafael Marcelo
+ *
+ */
+public interface InstrumentoRepository extends CrudRepository<Instrumento, Integer> {
 }
